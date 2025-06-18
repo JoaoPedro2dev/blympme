4c0640d567012d1698a025244ec5a665b663eca9
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-06-18 18:32:15 -0300

    .htaccess
    Adicionando .htaccess para o controle de rotas

--- API/index.php
+++ API/index.php
@@ -1,3 +1,22 @@
 <?php 
-    echo "Ola mundo";
+    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $base = '/blympme/API';
+
+    $url = str_replace($base, '', $url);
+
+    // echo $url."<br>";
+    
+    switch($url){
+        case '/':
+            echo "VocÊ esta no index";
+            break;
+        case '/tasks':
+            echo "Aqui você pode listar as task";
+            break;
+        case '/cadastrartask':
+            echo "Aqui você pode cadastrar tasks";
+            break;
+        default: echo 'not Found';
+    }
 ?>
