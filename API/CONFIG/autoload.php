9ab897feb351a7d62df92f601c700228a5e0ff66
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-06-25 14:00:10 -0300

    Routes e Functions
    separando Routes do index para mais organização. Modelando functions para ser uma classe e manter a POO

---
+++ API/CONFIG/autoload.php
@@ -0,0 +1,14 @@
+<?php 
+    define('BASE_DIR', __DIR__);
+
+    spl_autoload_register(function ($nome_da_classe)
+    {
+       $arquivo = BASE_DIR . "../../" . $nome_da_classe . ".php";
+       
+       if(file_exists($arquivo)){
+        include $arquivo;
+       }else{
+        throw new Exception("Arquivo não encontrado");
+       }
+    });
+?>
--- API/HELP/functions.php
+++ API/HELP/functions.php
@@ -1,23 +1,34 @@
 <?php 
-    function verifyKey(){
-        if(!isset($_GET['key']) || empty($_GET['key'])){
-            die(json_encode(['status' => 'error', 'descricao' => 'voce precisa de uma cave de acesso']));
+    namespace Help;
+
+    abstract class functions{
+        public static function verifyKey(){
+            if(!isset($_GET['key']) || empty($_GET['key'])){
+                die(json_encode(['status' => 'error', 'descricao' => 'voce precisa de uma chave de acesso']));
+            }
+
+            if($_GET['key'] !== $_ENV['key']){
+                die(json_encode(['status' => 'error', 'descricao' => 'chave de acesso nao reconhecida']));
+            }
         }
 
-        if($_GET['key'] !== $_ENV['key']){
-            die(json_encode(['status' => 'error', 'descricao' => 'chave de acesso nao reconhecida']));
+        public static function varLenght($var, int $max, string $descricao){
+            if(mb_strlen($var) > $max){
+                    die(json_encode(['status' => 'erro', 'descricao' => $descricao]));
+            }
         }
-    }
 
-    function varLenght($var, int $max, string $descricao){
-        if(mb_strlen($var) > $max){
-                die(json_encode(['status' => 'erro', 'descricao' => $descricao]));
+        public static function typeId($id){
+            if(!filter_var($id, FILTER_VALIDATE_INT)){
+                die(json_encode(['status' => 'erro', 'descricao' => 'digite um id valido']));
+            }
         }
-    }
 
-    function typeId($id){
-        if(!filter_var($id, FILTER_VALIDATE_INT)){
-            die(json_encode(['status' => 'erro', 'descricao' => 'digite um id valido']));
+        public static function formatUrl(){
+            $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            $base = '/blympme/API';
+            
+            return $url = str_replace($base, '', $url);
         }
-    }
+    }  
 ?>
